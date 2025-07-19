ec3868c2cb6763fbcbb3359f81072d4022c3b764
Luka Radulovic <luka.91.rad@gmail.com>
2025-07-19 23:06:23 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/habits-simple-pwa/sw.js', { scope: '/habits-simple-pwa/' })})}
