67bbe203aa1b71f6827242900b0fbe599881c7e1
Luka Radulovic <luka.91.rad@gmail.com>
2025-07-20 13:49:42 +0200

    [master] test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: "/habits-simple-pwa/",
-  plugins: [react()],
+  base: '/habits-simple-pwa/', // ⚠️ OBAVEZNO!
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Habits Simple PWA',
+        short_name: 'HabitsPWA',
+        theme_color: '#121212',
+        background_color: '#121212',
+        display: 'standalone',
+        start_url: '/habits-simple-pwa/',
+        icons: [
+          {
+            src: 'rsz_2navike.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'navike.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
